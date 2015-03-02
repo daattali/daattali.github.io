49d091ec393f04a3203c65d539b2709eba6474ee
Dean Attali <daattali@gmail.com>
2015-03-01 23:51:28 -0800

    more fixes to css and js

--- js/main.js
+++ js/main.js
@@ -1,7 +1,15 @@
 $(window).scroll(function() {
     if ($(".navbar").offset().top > 50) {
-        $(".navbar").addClass("top-nav-collapse");
+        $(".navbar").addClass("top-nav-short");
     } else {
-        $(".navbar").removeClass("top-nav-collapse");
+        $(".navbar").removeClass("top-nav-short");
     }
 });
+
+$('#main-navbar').on('show.bs.collapse', function () {
+  $(".navbar").addClass("top-nav-expanded");
+})
+
+$('#main-navbar').on('hidden.bs.collapse', function () {
+  $(".navbar").removeClass("top-nav-expanded");
+})
