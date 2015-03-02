35eae568c22ccf0d9ef9103d87c8c55319a7c886
Dean Attali <daattali@gmail.com>
2015-03-01 20:45:47 -0800

    add navbar

--- js/main.js
+++ js/main.js
@@ -1,1 +1,7 @@
-alert("test");
+$(window).scroll(function() {
+    if ($(".navbar").offset().top > 50) {
+        $(".navbar").addClass("top-nav-collapse");
+    } else {
+        $(".navbar").removeClass("top-nav-collapse");
+    }
+});
