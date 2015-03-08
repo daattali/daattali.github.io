082b2d84b8ff71030d7828025b96188b242f11fd
Dean Attali <daattali@gmail.com>
2015-03-07 17:55:53 -0800

    add a separate meta title+description

--- js/index.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-  $(".page-heading h1").typed({
-    strings : ["Hi, I'm Dean"],
-	typeSpeed : 50,
-	callback: function() { $(".page-heading").toggleClass("move-up"); }
-  });
-  
-});
