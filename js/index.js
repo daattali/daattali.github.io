d313ea718e74e4414837b0cb8e1b522c832346b2
Dean Attali <daattali@gmail.com>
2015-03-07 17:26:12 -0800

    improve typed cursor

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
   $(".page-heading h1").typed({
     strings : ["Hi, I'm Dean"],
+	typeSpeed : 50,
 	callback: function() { $(".page-heading").toggleClass("move-up"); }
   });
   
