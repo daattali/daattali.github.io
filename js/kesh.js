86aeacf480081e0d47a893d2ea7c5e51d2c60e2c
Dean Attali <daattali@gmail.com>
2015-03-09 11:19:37 -0700

    fix resume typo and refactor kesh js page

--- js/kesh.js
+++ js/kesh.js
@@ -1,77 +1,80 @@
-var kisstimeout = null;
-var kisssound = null;
-var kissimg = null;
+var keshPage = {
 
-var init = function() {
-  $("#nokesh").click(function() {
-  	$("#page-initial").hide();
-  	$("#page-nokesh").show();
-  });
+  kisstimeout : null,
+  kisssound : null,
+  kissimg : null,
 
-  $("#yeskesh").click(function() {
-  	$("#page-initial").hide();
-  	$("#page-yeskesh").show();
-  });
+  init : function() {
+	$("#nokesh").click(function() {
+	  $("#page-initial").hide();
+	  $("#page-nokesh").show();
+	});
 
-  $("#submitqs").click(function() {
-  	$("#lies").hide();
+	$("#yeskesh").click(function() {
+	  $("#page-initial").hide();
+	  $("#page-yeskesh").show();
+	});
 
-  	var clickesyes = false;
-  	$("#verifyqs .no").each(function(idx){
-  	  if ($(this)[0].checked) clickesyes = true;
-  	})
-  	if (!$("#verifyqs .yes")[0].checked) {
-  		clickesyes = true;
-  	}
+	$("#submitqs").click(function() {
+	  $("#lies").hide();
 
-  	if (clickesyes) {
-  	  $("#lies").fadeIn(500);
-  	} else {
-  	  $("#page-initial").hide();
-  	  $("#page-yeskesh").hide();
-  	  $("#page-vday").show();
-	  $('.carousel').carousel({
-	    interval: 4000
+	  var clickesyes = false;
+	  $("#verifyqs .no").each(function(idx){
+	    if ($(this)[0].checked) clickesyes = true;
+	  })
+	  if (!$("#verifyqs .yes")[0].checked) {
+		clickesyes = true;
+	  }
+
+  	  if (clickesyes) {
+	    $("#lies").fadeIn(500);
+	  } else {
+	    $("#page-initial").hide();
+	    $("#page-yeskesh").hide();
+	    $("#page-vday").show();
+	    $('.carousel').carousel({
+		  interval: 4000
+	    });
+	    $("#vdaymsg-txt").typed({
+		  strings: ["Sorry about those questions, I just had to make sure :p",
+			  	    "I'm probably on a plane right now, so I hope this can help you feel my love from thousands of miles away.<br/>" +
+				    "You must be running on a dangerously low level of kisses right now, so I tried to make sure you get your dosage."],
+		  backDelay: 2500,
+		  typeSpeed: 0,
+		  callback : function() { $("#kissbtn").show(); }
+	    });
+	  }
+	});
+
+	kissimg = $("#kissimg");
+	kisssound = $("#kisssound")[0];
+	$("#kissbtn").click(function() {
+	  kisssound.pause();
+	  kisssound.currentTime = 0;
+	  kisssound.play();
+
+	  var img = $(document.createElement("img"));
+	  img.attr('src', 'img/kesh/kiss.jpg');
+	  var size = Math.round(Math.random() * 300 + 100);  // kiss image size 100-400 px
+	  var rotate = Math.round(Math.random() * 360);
+	  var left = Math.round(Math.random() * (window.innerWidth - size));
+	  var top = Math.round(Math.random() * (window.innerHeight - size));
+	  img.css({
+	    'width' : size,
+	    'left' : left,
+	    'top' : top,
+	    'transform' : 'rotate(' + rotate + 'deg)'
 	  });
-	  $("#vdaymsg-txt").typed({
-	  	strings: ["Sorry about those questions, I just had to make sure :p",
-	  			  "I'm probably on a plane right now, so I hope this can help you feel my love from thousands of miles away.<br/>" +
-	  			  "You must be running on a dangerously low level of kisses right now, so I tried to make sure you get your dosage."],
-	    backDelay: 2500,
-	    typeSpeed: 0,
-	    callback : function() { $("#kissbtn").show(); }
+	  img.addClass('kissimg');
+	  document.body.appendChild(img[0]);
+	  img.hide().fadeIn(600, function() {
+	    kisstimeout = setTimeout(
+		  function() { img.hide(); },
+		  1000
+	    );
 	  });
-  	}
-  });
-
-  kissimg = $("#kissimg");
-  kisssound = $("#kisssound")[0];
-  $("#kissbtn").click(function() {
-  	kisssound.pause();
-  	kisssound.currentTime = 0;
-  	kisssound.play();
-
-  	var img = $(document.createElement("img"));
-  	img.attr('src', 'img/kesh/kiss.jpg');
-  	var size = Math.round(Math.random() * 300 + 100);  // kiss image size 100-400 px
-  	var rotate = Math.round(Math.random() * 360);
-  	var left = Math.round(Math.random() * (window.innerWidth - size));
-  	var top = Math.round(Math.random() * (window.innerHeight - size));
-  	img.css({
-      'width' : size,
-  	  'left' : left,
-  	  'top' : top,
-  	  'transform' : 'rotate(' + rotate + 'deg)'
-  	});
-  	img.addClass('kissimg');
-  	document.body.appendChild(img[0]);
-   	img.hide().fadeIn(600, function() {
-  	  kisstimeout = setTimeout(
-  	  	function() { img.hide(); },
-  	  	1000
-  	  );
-  	});
-  });
+	});
+  }
 };
 
-document.addEventListener('DOMContentLoaded', init);
+document.addEventListener('DOMContentLoaded', keshPage.init);
